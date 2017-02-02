268307b04ecd620fb38612e9accdbcb17dd7b4b6
Gordon <geniusgordon@gmail.com>
2017-02-02 18:48:16 +0800

    saveHistory bug

--- src/history.js
+++ src/history.js
@@ -4,9 +4,13 @@ import path from 'path';
 const historyFileName = path.resolve(process.env.HOME, '.cangjie_history');
 
 export const loadHistory = async () => {
-  const history = await new Promise((resolve, reject) =>
-    fs.readFile(historyFileName, (err, data) => err ? reject(err) : resolve(data)));
-  return history.toString().split('');
+  try {
+    const history = await new Promise((resolve, reject) =>
+      fs.readFile(historyFileName, (err, data) => err ? reject(err) : resolve(data)));
+    return history.toString().split('');
+  } catch (err) {
+    return [];
+  }
 };
 
 export const saveHistory = word =>
